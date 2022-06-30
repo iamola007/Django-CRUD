f718bc5baae99fd33c463e1b49b29874d4ed22cb
Ola Oladapo <olaoladapo007@gmail.com>
2022-06-30 12:44:49 +0100

    update views

---
+++ I4G002734ZZ3/blog/views.py
@@ -0,0 +1,10 @@
+import imp
+from django.views.generic import ListView
+from .models import Post
+
+# Create your views here.
+
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
